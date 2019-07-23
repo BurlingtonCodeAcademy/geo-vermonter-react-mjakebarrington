b912ffcfadd4e7a7a0ec85b7a1f694705a033203
Jake Barrington <mjakebarrington@gmail.com>
2019-07-23 16:25:25 -0400

    Added Icons

---
+++ src/leaflet.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+class Leaflet extends React.Component {
+    render() {
+        return (
+            <div id="mapid"></div>
+        );
+    }
+}
+
+export default Leaflet; 
