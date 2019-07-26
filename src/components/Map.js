5174de957de00e5f8395cb60b33bed934ba583ca
Jake Barrington <mjakebarrington@gmail.com>
2019-07-26 14:52:49 -0400

    well I fixed the map, but everything else broke

--- src/components/Map.js
+++ src/components/Map.js
@@ -14,7 +14,7 @@ class Map extends React.Component {
   }
 
   componentDidMount() {
-    let map = L.map('map').setView([this.props.lat, this.props.lon], 13);
+    let map = L.map('map').setView([this.state.lat, this.state.lon], 18);
     L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
       attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
     }).addTo(map);
