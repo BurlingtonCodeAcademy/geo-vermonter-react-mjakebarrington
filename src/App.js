01ba2626777953fc459264ee1900ad4266ad0c85
Jake Barrington <mjakebarrington@gmail.com>
2019-07-26 08:41:53 -0400

    changed to state within Map comp, added functionality template

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import './App.css';
 import L from 'leaflet';
 import Map from './components/Map'
-import Controls from './components/Controls'
 
 function App() {
   return (
     <div>
     <Map/>
-    <Controls/>
     </div>
   );
 }
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,36 +1,100 @@
-import React from 'react';
-import './Map.css'
-import L from 'leaflet';
-
+import React from "react";
+import "./Map.css";
+import "./Controls.css";
+import L from "leaflet";
 
 class Map extends React.Component {
-    componentDidMount() {
-        this.map = L.map("map", {
-            //Start on Burlington Code Academy
-            center: [44.4759406, -73.2123868],
-            zoom: 18,
-            maxZoom: 18,
-            minZoom: 18,
-            layers: [
-              L.tileLayer(
-                "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
-                {
-                  attribution:
-                    "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
-                }
-              )
-            ]
-          })
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    // Starting pos for BCA.
+    gamestate: false,
+    lat: 44.4759406,
+    lon: -73.2123868,
+    map: L.map("map", {
+    center: [this.lat, this.lon],
+    zoom: 18,
+    maxZoom: 19,
+    minZoom: 18,
+    layers: [
+        L.tileLayer(
+          "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+          {
+            attribution:
+              "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+          }
+        )
+      ]
+    })
+    };
+
+    // Binding functions to state.
+    this.start = this.start.bind(this);
+    this.end = this.end.bind(this);
+    this.plop = this.plop.bind(this);
+    this.rLon = this.rLon.bind(this);
+    this.rLat = this.rLat.bind(this);
+    this.moveNorth = this.moveNorth.bind(this);
+    this.moveSouth = this.moveSouth.bind(this);
+    this.moveEast = this.moveEast.bind(this);
+    this.moveWest = this.moveWest.bind(this);
+  }
+
+start() {
+  // Start the game
 }
-render() {
+
+end() {
+  // End the Game
+}
+
+plop() {
+  // Plops the user
+}
+
+rLon() {
+  // Random longitude
+}
+
+rLat() {
+  // Random latitude
+}
+
+moveNorth() {
+  // Move map North
+}
+
+moveSouth() {
+  // Move map South
+}
+
+moveEast() {
+  // Move map East
+}
+
+moveWest() {
+  // Move map West
+}
+
+  render() {
     return (
-        <div>
+      <div>
         <br />
         <br />
-        <div id="map"></div>
+        <div id="map" lat={this.props.lat} lon={this.props.lon} />
+        <div id="Controls" class="grid-container">
+          <div id="North" class="North">
+            N
+          </div>
+          <div class="South">S</div>
+          <div class="East">E</div>
+          <div class="West">W</div>
+          <div class="Button"> </div>
         </div>
+      </div>
     );
+  }
 }
-};
 
 export default Map;
