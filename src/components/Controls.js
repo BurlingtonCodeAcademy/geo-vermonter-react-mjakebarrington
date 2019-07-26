a90195daada6bb9fdb476af89a165b944dcb59c7
Jake Barrington <mjakebarrington@gmail.com>
2019-07-25 21:01:30 -0400

    basic controls

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import './Map.css';
-import '../App.css';
-
+import React from "react";
+import "./Map.css";
+import "../App.css";
+import "./Controls.css";
 
 export default function Controls() {
-        return (
-             <p>Hello!</p>
-        );
+  return (
+    <div id="Controls" class="grid-container">
+      <div class="North">N</div>
+      <div class="South">S</div>
+      <div class="East">E</div>
+      <div class="West">W</div>
+      <div class="Button"> </div>
+    </div>
+  );
 }
